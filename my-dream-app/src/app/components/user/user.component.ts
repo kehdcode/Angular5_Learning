4d9b9cbcdbe2c6afaa2325215b5b6d9ffca187ed
github_kehdcode <kehindedawood@gmail.com.com>
2018-08-16 21:05:03 +0100

    string interpollation, data binding, forloop

--- my-dream-app/src/app/components/user/user.component.ts
+++ my-dream-app/src/app/components/user/user.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  name:string;
+  age: number;
+  sex: any;
+  address: Address;
+  hobbies: string[];
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.name = " Kehdcode";
+    this.age= 23;
+    this.sex= "male";
+    this.address={
+      street: "22b john bash str",
+      city: "lagos",
+      state: "Lagos"
 
+    }
+    this.hobbies=["eating", "skeeping", "reading"];
+  }
+ 
+}
+interface Address {
+  street: string;
+  city: string;
+  state: string;
 }
