85a04afe61d736a2edab3e929269a686582094d6
github_kehdcode <kehindedawood@gmail.com.com>
2018-08-17 00:00:37 +0100

    two way data binding using ngModel by importing formModules

--- my-dream-app/src/app/components/user/user.component.ts
+++ my-dream-app/src/app/components/user/user.component.ts
@@ -25,8 +25,26 @@ export class UserComponent implements OnInit {
 
     }
     this.hobbies=["eating", "skeeping", "reading"];
+
+  
   }
- 
+  onClick(){
+    this.name ='Dawood Ahmed Kehinde';
+    this.hobbies.pop();
+  }
+ addHobby(hobby){
+   this.hobbies.unshift(hobby);
+   return false;
+ }
+ deleteHobby(hobby){
+   for( let i=0; i< this.hobbies.length; i++ ){
+     if(this.hobbies[i] == hobby){
+       this.hobbies.splice(i, 1)
+
+     }
+
+   }
+ }
 }
 interface Address {
   street: string;
